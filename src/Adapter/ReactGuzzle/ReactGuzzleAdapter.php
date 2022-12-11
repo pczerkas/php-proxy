d0eb361179153544882a9b6140a3c072f358d0e8
Przemysław Czerkas <pczerkas@gmail.com>
2022-12-11 10:44:46 +0100

    Add ReactGuzzleAdapter

---
+++ src/Adapter/ReactGuzzle/ReactGuzzleAdapter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Proxy\Adapter\React;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use Proxy\Adapter\AdapterInterface;
+use Psr\Http\Message\RequestInterface;
+
+class ReactGuzzleAdapter implements AdapterInterface
+{
+    /**
+     * The Guzzle client instance.
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * Construct Guzzle based HTTP adapter.
+     * @param Client $client
+     */
+    public function __construct(Client $client = null)
+    {
+        $this->client = $client ?: new Client;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function send(RequestInterface $request)
+    {
+        $request = new Request(
+            $request->getMethod(),
+            (string) $request->getUri(),
+            $request->getHeaders(),
+            (string) $request->getBody()
+        );
+
+        $promise = $this->client->sendAsync($request);
+
+        $response = $promise->wait();
+
+        return $response;
+    }
+}
